c4553f3271d012ae2a04f6529a035ce2f04201ca
useris <dominykas.kruminas@gmail.com>
2025-03-12 14:51:07 +0200

    daily

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,17 +5,20 @@ function Home() {
   return (
     <div>
 
+    {/*Header*/}
     <div>
      <h1 className="Header bg-dark text-white d-flex justify-content-center mt-0 w-100">The Aesthetic Essential &nbsp; &nbsp; &nbsp; ⚝</h1>   
     </div>
 
+    {/*Buttons*/}
     <div className="min-vh-100 bg-black d-flex align-items-center justify-content-center gap-3 ">
-    <button type="button" class="btn btn-primary">Video</button>
-    <button type="button" class="btn btn-secondary">Video</button>
-    <button type="button" class="btn btn-success">Video</button>
-    <button type="button" class="btn btn-danger">Video</button>
-    </div>
+    <button type="button" class="btn btn-primary">Aesthetic</button>
+    <button type="button" class="btn btn-secondary">Sekiro</button>
+    <button type="button" class="btn btn-success">Workout</button>
+    <button type="button" class="btn btn-danger">Energy</button>
     
+    </div>
+
     </div>
   );
 }
